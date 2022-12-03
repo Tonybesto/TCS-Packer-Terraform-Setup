5e25af702db96389a917fc6ef7d1055279b25d0b
Akoji Anthony <akojianthony@gmail.com>
2022-12-03 15:24:07 +0100

    add AMI ID

--- terraform/main.tf
+++ terraform/main.tf
@@ -70,12 +70,12 @@ module "security" {
 
 module "Autoscaling" {
   source            = "./modules/Autoscaling"
-  ami-web           = var.ami
-  ami-bastion       = var.ami
-  ami-nginx         = var.ami
-  desired_capacity  = 2
-  min_size          = 2
-  max_size          = 2
+  ami-web           = var.ami-web
+  ami-bastion       = var.ami-bastion
+  ami-nginx         = var.ami-nginx
+  desired_capacity  = 1
+  min_size          = 1
+  max_size          = 1
   web-sg            = [module.security.web-sg]
   bastion-sg        = [module.security.bastion-sg]
   nginx-sg          = [module.security.nginx-sg]
