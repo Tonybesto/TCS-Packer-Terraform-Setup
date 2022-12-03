fea3dc17bee5faa7f509e55a3833400a2aaff006
Akoji Anthony <akojianthony@gmail.com>
2022-12-03 21:25:30 +0100

    remove s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,24 @@
 ##creating bucket for s3 backend
 #########################
 
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "pbl18"
-  force_destroy = true
-}
-resource "aws_s3_bucket_versioning" "version" {
-  bucket = aws_s3_bucket.terraform-state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "first" {
-  bucket = aws_s3_bucket.terraform-state.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+# resource "aws_s3_bucket" "terraform-state" {
+#   bucket = "pbl18"
+#   force_destroy = true
+# }
+# resource "aws_s3_bucket_versioning" "version" {
+#   bucket = aws_s3_bucket.terraform-state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+# resource "aws_s3_bucket_server_side_encryption_configuration" "first" {
+#   bucket = aws_s3_bucket.terraform-state.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "terraform-locks"
